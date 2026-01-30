0afada1ad4063692cb31df1468e2f29ad7e0f378
Adzrul Danin <adzruldani@gmail.com>
2026-01-30 23:30:21 +0800

    Fixed deployment issue

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-from google.cloud.sql.connector import Connector
 
 load_dotenv()
 
@@ -13,29 +12,38 @@ DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_PORT = os.getenv("DATABASE_PORT")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
-DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-connector = Connector()
-
-def getconn():
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pg8000",
-        user=DATABASE_USER,
-        password=DATABASE_PASSWORD,
-        db=DATABASE_NAME
+if INSTANCE_CONNECTION_NAME and os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
+    from google.cloud.sql.connector import Connector
+    connector = Connector()
+    
+    def getconn():
+        conn = connector.connect(
+            INSTANCE_CONNECTION_NAME,
+            "pg8000",
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD,
+            db=DATABASE_NAME
+        )
+        return conn
+    
+    engine = create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        pool_size=5,
+        max_overflow=10,
+        pool_timeout=100,
+        pool_recycle=1800
     )
-    return conn
-
-engine = create_engine(
-    DATABASE_URL,
-    # "postgresql+pg8000://",
-    # creator=getconn,
-    pool_size=5,
-    max_overflow=10,
-    pool_timeout=100,
-    pool_recycle=1800
-)
+else:
+    DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    engine = create_engine(
+        DATABASE_URL,
+        pool_size=5,
+        max_overflow=10,
+        pool_timeout=100,
+        pool_recycle=1800
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
